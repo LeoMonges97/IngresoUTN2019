b62df62cfc6c21787fd06a8d39fc2b2069f65abe
LeoMonges97 <47321295+LeoMonges97@users.noreply.github.com>
2019-02-10 23:21:53 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-	
+
+var importe;
+var descuento;
+var total;
+
+importe = parseFloat (document.getElementById("importe").value);
+
+descuento = importe * 25 / 100
+
+total = importe - descuento;
+
+alert ("El descuento es de " + descuento + "%");
+
+document.getElementById("resultado").value = total;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+    
+var sueldo;
+var x;
+var nuevosueldo;
 
-	
+sueldo = parseFloat (document.getElementById("sueldo").value);
+
+x = sueldo * 10 / 100;
+
+nuevosueldo = sueldo + x;
+
+alert ("El aumento es de " + x);
+
+document.getElementById("resultado").value = nuevosueldo;
+
+
 }
