77b9e0f00624b77d1b119aff2e5f61cd8c647ead
LeoMonges97 <47321295+LeoMonges97@users.noreply.github.com>
2019-02-10 23:23:42 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,16 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+var numb1;
+var numb2;
+var suma;
+
+numb1 = parseint (document.getElementById("numeroUno").value);
+numb1 = parseint (document.getElementById("numeroDos").value);
+
+suma = numb1 + numb2;
+
+alert ("El resultado de la suma es: " + suma);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,14 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+var numb1;
+var numb2;
+var division;
+
+numb1 = document.getElementById("numeroDividendo").value;
+numb2 = document.getElementById("numeroDivisor").value;
+
+division = numb1 % numb2;
+
+alert ("El resultado es " + division);
 }
